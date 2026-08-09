6ddf44b636fa40c75e04344a3cbf16bbdb481bbd
Salem Alsaiair <s@sy.sa>
2026-08-09 05:31:30 +0300

    refactor: remove top bar

--- src/shell/layout.tsx
+++ src/shell/layout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "./sidebar";
-import { TopBar } from "./top-bar";
 import { TaskLog } from "./task-log";
 
 export function Shell() {
   return (
     <div className="flex h-screen flex-col" data-testid="shell">
-      <TopBar />
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
         <main className="flex-1 overflow-auto bg-surface-950">
